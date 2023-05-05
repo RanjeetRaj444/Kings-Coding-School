15825bbee6eb30cd6df49eb9f141611196e11923
ranjeetraj444 <ranjeetraj44666@gmail.com>
2023-05-06 01:01:38 +0530

    day3

--- project3/src/context/Context.jsx
+++ project3/src/context/Context.jsx
@@ -1,7 +1,8 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export let AppContext = createContext();
 export default function Context({ children }) {
+  const [state, setState] = useState([]);
   console.log("Context");
   const name = "Ranjeet";
   return <AppContext.Provider value={{ name }}>{children}</AppContext.Provider>;
